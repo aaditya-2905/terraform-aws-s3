9c2093be952ddeede067ea55703f8736212af6be
Aadityasinh zala <zalaaadityasinh9@gmail.com>
2026-04-27 12:41:20 +0530

    Changed outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "bucket_id" {
 output "bucket_arn" {
   value = module.s3.bucket_arn
 }
+
+output "bucket_domain_name" {
+  value = module.s3.bucket_domain_name
+}
+
+output "bucket_regional_domain_name" {
+  value = module.s3.bucket_regional_domain_name
+}
